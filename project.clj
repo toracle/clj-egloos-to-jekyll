886cffe0db9effafba9037dd6cf477fc04e913bd
Jeongsoo Park <toracle@gmail.com>
2015-09-21 13:07:56 +0900

    Add an enlive dependency

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject clj-egloos-dump "1.0.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [http-kit "2.1.18"]]
+                 [http-kit "2.1.18"]
+                 [enlive "1.1.6"]]
   :profiles {:dev {:plugins [[lein-midje "3.1.1"]]}})
