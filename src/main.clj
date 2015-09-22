7de9314a53716e21a081622218933f9516f31c0c
Jeongsoo Park <toracle@gmail.com>
2015-09-22 22:26:04 +0900

    .

--- src/main.clj
+++ src/main.clj
@@ -8,10 +8,19 @@
 
 (def target-egloos-url "http://toracle.egloos.com")
 
+(def content-list-dir "data/content-list/")
+
+(def articles-url-data-path "data/article-urls.dat")
+
+(def articles-dir "data/article/")
+
+(def articles-output-dir "data/article-markdown/")
+
+
 ;; Get Content List
 
 (defn content-list-path [filename]
-  (str "data/content-list/" filename))
+  (str content-list-dir filename))
 
 (defn target-egloos-page-url [page-num]
   (str target-egloos-url "/page/" page-num))
@@ -34,7 +43,8 @@
                                 (target-egloos-page-url page-num))
                               (range 1 (+ total-page 1)))]
     (doseq [url content-list-urls]
-      (save-webpage-to-file url (content-list-path (str "page-" (get-page-num-from-url url) ".html"))))))
+      (save-webpage-to-file url (content-list-path (format "page-%s.html"
+                                                           (get-page-num-from-url url)))))))
 
 ;; Get Article List
 
@@ -50,7 +60,7 @@
 
 (defn get-article-urls []
   (let [data-files (map (fn [page-num]
-                          (content-list-path (str "page-" page-num ".html")))
+                          (content-list-path (format "page-%s.html" page-num)))
                         (range 1 (+ total-page 1)))]
     (flatten
      (map (fn [filename]
@@ -59,29 +69,59 @@
 
 (defn save-article-urls []
   (let [article-urls (get-article-urls)]
-    (spit "data/article-urls.dat" (prn-str article-urls))))
+    (spit articles-url-data-path (prn-str article-urls))))
 
 (defn save-all-article []
   (let
-      [article-urls (read-string(slurp "data/article-urls.dat"))]
+      [article-urls (read-string(slurp articles-url-data-path))]
     (map (fn [url]
-           (save-webpage-to-file url (str "data/article/article-" (get-page-num-from-url url) ".html")))
+           (save-webpage-to-file url (format "%sarticle-%s.html"
+                                             articles-dir
+                                             (get-page-num-from-url url))))
          article-urls))
   true)
 
-(defn convert-to-markdown-dom [root]
+(defn convert-dom-to-markdown [root]
   (let
       [content (map (fn [element]
-                      (cond (string? element) element
-                            (= (:tag element) :br) "\n"
-                            (= (:tag element) :a) (format "[%s](%s)" (:href (:attrs element)) (html/text element))
-                            :else nil))
+                      (let [tag (:tag element)]
+                        (cond (string? element) element
+                              (= tag :br) "\n"
+                              (= tag :b) (format "**%s**"
+                                                 (html/text element))
+                              (= tag :a) (if
+                                             (nil? (:content element))
+                                           ""
+                                           (format "[%s](%s)"
+                                                   (convert-dom-to-markdown (:content element))
+                                                   (:href (:attrs element))))
+                              (= tag :img) (do
+                                             (format "[%s]"
+                                                     (:src (:attrs element))))
+                              (= tag :div) (convert-dom-to-markdown (:content element))
+                              (= tag :blockquote) (format "> %s"
+                                                          (convert-dom-to-markdown (:content element)))
+                              (= tag :font) (convert-dom-to-markdown (:content element))
+                              (= tag :span) (convert-dom-to-markdown (:content element))
+                              (= tag :p) (format "\n\n%s\n\n"
+                                                 (convert-dom-to-markdown (:content element)))
+                              (= tag :hr) "***"
+                              :else (clojure.string/trim (html/text element)))))
                     root)]
     (->> content
          flatten
          (remove nil?)
-         pr-str
          (clojure.string/join ""))))
+
+(defn slugify [s]
+  (-> s
+      (clojure.string/replace " " "-")
+      (clojure.string/replace "," "")
+      (clojure.string/replace "[" "")
+      (clojure.string/replace "]" "")
+      (clojure.string/replace "?" "")
+      (clojure.string/replace ":" "-")
+      (clojure.string/replace "." "")))
 
 (defn parse-article-content [content]
   {:title (-> content
@@ -89,17 +129,58 @@
               html/html-resource
               (html/select [:h2.entry-title :a])
               first
-              html/text),
+              html/text)
+   :title-slug (-> content
+              java.io.StringReader.
+              html/html-resource
+              (html/select [:h2.entry-title :a])
+              first
+              html/text
+              slugify)
+   :datetime (-> content
+             java.io.StringReader.
+             html/html-resource
+             (html/select [:li.post_info_date :abbr])
+             first
+             html/text
+             (clojure.string/replace "/" "-"))
    :date (-> content
              java.io.StringReader.
              html/html-resource
              (html/select [:li.post_info_date :abbr])
              first
-             html/text)
+             html/text
+             (clojure.string/replace "/" "-")
+             (subs 0 10))
    :content (-> content
                 java.io.StringReader.
                 html/html-resource
                 (html/select [:div.post_content :div.hentry])
                 first
                 :content
-                )})
+                convert-dom-to-markdown
+                )
+   :category (-> content
+                 java.io.StringReader.
+                 html/html-resource
+                 (html/select [:span.post_title_category :a])
+                 first
+                 html/text)})
+
+(defn convert-parsed-to-markdown-article [m]
+  (format "---\nlayout: post\ntitle: \"%s\"\ndate: %s\ncategories: %s\n---\n\n%s"
+          (:title m) (:datetime m) (:category m) (:content m)))
+
+(defn convert-all-article-to-markdown []
+  (let [article-file-list (rest (file-seq (java.io.File. articles-dir)))]
+    (map (fn [file]
+           (let [parsed (parse-article-content (slurp file))
+                 body (convert-parsed-to-markdown-article parsed)
+                 out-file (java.io.File. (format "%s%s-%s.md"
+                                                 articles-output-dir
+                                                 (:date parsed)
+                                                 (:title-slug parsed)))]
+             (.mkdirs (.getParentFile out-file))
+             (with-open [wrtr (io/writer out-file)]
+               (.write wrtr body))))
+          article-file-list)))
